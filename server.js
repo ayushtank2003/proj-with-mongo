e89f5261f444673c9d2960e954f8dcdddf7b24f1
ayushtank2003 <ayushtaank2003@gmail.com>
2024-08-04 04:37:37 +0530

    new

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express =require("express");
+const mongoose =require("mongoose") 
 const userRouter = require("./routes/userRouter");
 const app = express();
 const port=8000;
@@ -7,6 +8,11 @@ app.use("/user",  userRouter);
 
 const server = app.listen(port,()=>{
     console.log(`server is running at port${port}`);
+    
+});
+
+const DB =mongoose.connect("mongodb+srv://ayush:kLtVxBEhSTu3ZFIq@cluster0.lqes229.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("connect to database");
     
 });
 
